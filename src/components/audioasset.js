8ee2da3d4a0fc74073da37dbae4c8d0de7ce4fe9
kurorinchan <kurorinchan@users.noreply.github.com>
2020-03-23 23:10:23 +0900

    Barebones of AudioAssetGroup and AssetFinder
    AudioAssetGroup is a bit tested.

--- src/components/audioasset.js
+++ src/components/audioasset.js
@@ -1,26 +1,324 @@
 
 const fs = require('fs')
+const path = require('path')
+
+const SHIKO = 'shiko'
+const VOICE = 'voice'
+
+// These are the directory names that it should be looking for.
+const FAST_DIR = 'fast'
+const FINISH_DIR = 'finish'
+const NORMAL_DIR = 'nomal'
+const START_DIR = 'start'
+const SILENCE_DIR = 'muon'
+const NORMAL_PHRASE_DIR = 'nomal_s'
+
+// Volume realted constants.
+const INIT_VOLUME = 100
+const MAX_VOLUME = 100
+const MIN_VOLUME = 0
+
+function clampVolume(volume) {
+  return Math.max(MIN_VOLUME, Math.min(MAX_VOLUME, volume))
+}
+
+function exploreDirForPrefixes(dir, relevantPrefixes) {
+  const files = fs.readdirSync(dir)
+  relavantFiles = []
+  for (const filename of files) {
+    // If there are no relevant prefixes, then all of them are relevant.
+    const relevant =
+      relevantPrefixes.length == 0 ||
+      relevantPrefixes.reduce(
+        (accum, prefix) => accum | filename.startsWith(prefix), false)
+    if (!relevant) {
+      continue
+    }
+
+    if (path.extname(filename) != '.wav') {
+      continue
+    }
+
+    relavantFiles.push(path.join(dir, filename))
+  }
+  return relavantFiles
+}
+
+function exploreShicoFast(dir) {
+  return exploreDirForPrefixes(dir, ['f'])
+}
+
+function exploreShicoNormal(dir) {
+  return exploreDirForPrefixes(dir, [])
+}
+
+function exploreShicoFinish(dir) {
+  return exploreDirForPrefixes(dir, ['count', 'end', 'za', 'zb'])
+}
+
+function exploreShicoStart(dir) {
+  return exploreDirForPrefixes(dir, ['cdownxxx', 'cdown', 's', 'sf', 'sg', 'sz'])
+}
+
+function pickRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function allStartingWithAsPlayer(prefix, paths) {
+  const players = []
+  for (const filePath of paths) {
+    if (path.basename(filePath).startsWith(prefix)) {
+      players.push(new Player(filePath))
+    }
+  }
+  return players
+}
+
+class Player {
+  constructor(audioFilePath) {
+    this.path = audioFilePath
+    this.audio = null
+  }
+
+  play() {
+    this.audio.play()
+  }
+
+  setVolume(volume) {
+    this.audio.volume = volume
+  }
+
+  get pathForTesting() {
+    return this.path
+  }
+}
+
+function pathsToPlayers(paths) {
+  const players = []
+  for (const p of paths) {
+    players.push(new Player(p))
+  }
+  return players
+}
+
+const START_FAST_VOICE = 'startfast'
+const START_VOICE = 'start'
+const SHICO_VOICE = 'shico'
+const PHRASE_VOICE = 'phrase'
+const SILENCE_VOICE = 'silence'
+const ABRUPT_VOICE = 'abrupt'
+const GIVE_UP_VOICE = 'giveup'
+const LAST_MINUTE_VOICE = 'lastminute'
+const FAST_VOICE = 'fast'
+const SHICO_FAST_VOICE = 'shicofast'
+const COUNT_DOWN_VOICE = 'countdown'
+// This is the single file that has "5,4,3,2,1"
+const SINGLE_FILE_COUNT_DOWN_VOICE = 'single-file-count-down-voice'
+const END_VOICE = 'end'
+const EXIT_VOICE = 'exit'
+
+function exploreShicoDir(shicoDir) {
+  const files = fs.readdirSync(shicoDir)
+  let shicoFastPaths = null
+  let shicoNormalPaths = null
+  let shicoFinishPaths = null
+  let shicoStartPaths = null
+
+  for (const filename of files) {
+    switch (filename) {
+      case FAST_DIR:
+        shicoFastPaths = exploreShicoFast(path.join(shicoDir, filename))
+        break
+      case NORMAL_DIR:
+        shicoNormalPaths = exploreShicoNormal(path.join(shicoDir, filename))
+        break
+      case FINISH_DIR:
+        shicoFinishPaths = exploreShicoFinish(path.join(shicoDir, filename))
+        break
+      case START_DIR:
+        shicoStartPaths = exploreShicoStart(path.join(shicoDir, filename))
+        break
+    }
+  }
+
+  // TODO: Handle the case when one of them is not populated.
+  const shicoMapping = {}
+  shicoMapping[SHICO_FAST_VOICE] = pathsToPlayers(shicoFastPaths)
+  shicoMapping[SHICO_VOICE] = pathsToPlayers(shicoNormalPaths)
+
+  // TODO: Make a constant for the string.
+  const found = shicoFinishPaths.find(
+    function (element) {
+      path.basename(element) == 'count.wav'
+    }
+  )
+  // TODO: handle not found.
+  shicoMapping[SINGLE_FILE_COUNT_DOWN_VOICE] = [new Player(found)]
+
+  shicoMapping[EXIT_VOICE] = allStartingWithAsPlayer('end', shicoFinishPaths)
+  shicoMapping[END_VOICE] = allStartingWithAsPlayer('za', shicoFinishPaths)
+  shicoMapping[ABRUPT_VOICE] = allStartingWithAsPlayer('zb', shicoFinishPaths)
+
+  shicoMapping[LAST_MINUTE_VOICE] = allStartingWithAsPlayer('sf', shicoStartPaths)
+  shicoMapping[GIVE_UP_VOICE] = allStartingWithAsPlayer('sg', shicoStartPaths)
+  shicoMapping[START_FAST_VOICE] = allStartingWithAsPlayer('sz', shicoStartPaths)
+  shicoMapping[COUNT_DOWN_VOICE] = allStartingWithAsPlayer('cdown', shicoStartPaths)
+
+  const shicoStartPlayers = []
+  for (const filePath of shicoStartPaths) {
+    const filename = path.basename(filePath)
+    if (!filename.startsWith('s'))
+      continue
+
+    // These have the same 's' prefix. Ignore them.
+    const isOneOfOthers =
+      filename.startsWith('sf') ||
+      filename.startsWith('sg') ||
+      filename.startsWith('sz')
+    if (isOneOfOthers)
+      continue
+
+    shicoStartPlayers.push(new Player(filePath))
+  }
+  shicoMapping[START_VOICE] = shicoStartPlayers
+
+  return shicoMapping
+}
+
+function exploreVoiceDir(voiceDir) {
+  const files = fs.readdirSync(voiceDir)
+  let fastPaths = null
+  let silencePaths = null
+  let normalPaths = null
+
+  for (const filename of files) {
+    switch (filename) {
+      case FAST_DIR:
+        fastPaths = exploreDirForPrefixes(path.join(voiceDir, filename), ['vf'])
+        break
+      case SILENCE_DIR:
+        silencePaths = exploreDirForPrefixes(path.join(voiceDir, filename), [])
+        break
+      case NORMAL_PHRASE_DIR:
+        normalPaths = exploreDirForPrefixes(path.join(voiceDir, filename), ['v'])
+        break
+    }
+  }
+
+  const pharseMapping = {}
+  pharseMapping[FAST_VOICE] = pathsToPlayers(fastPaths)
+  pharseMapping[SILENCE_VOICE] = pathsToPlayers(silencePaths)
+  pharseMapping[PHRASE_VOICE] = pathsToPlayers(normalPaths)
+  return pharseMapping
+}
 
 class AudioAssetGroup {
-  constructor (shicoDir, voiceDir) {
+  constructor(shicoDir, voiceDir) {
     this.shicoDir = shicoDir
     this.voiceDir = voiceDir
+    this.shicoVolume = INIT_VOLUME
+    this.phraseVolume = INIT_VOLUME
+
+    const shicoMapping = exploreShicoDir(this.shicoDir)
+    const phraseMapping = exploreVoiceDir(this.voiceDir)
+    this.allPlayers = Object.assign(shicoMapping, phraseMapping)
+  }
+
+  assetGroupNumber() {
+    const dirname = path.basename(this.shicoDir)
+    const assetNumber = dirname.substr(SHIKO.length)
+    return parseInt(assetNumber)
+  }
+
+  setShicoVolume(volume) {
+    this.shicoVolume = clampVolume(volume)
+  }
+
+  setPharseVolume(volume) {
+    this.phraseVolume = clampVolume(volume)
+  }
+
+  startFast() {
+    return pickRandomElement(this.allPlayers[START_FAST_VOICE])
+  }
+
+  shico() {
+    return pickRandomElement(this.allPlayers[SHICO_VOICE])
+  }
+
+  phrase() {
+    return pickRandomElement(this.allPlayers[PHRASE_VOICE])
+  }
+
+  silence() {
+    return pickRandomElement(this.allPlayers[SILENCE_VOICE])
+  }
+
+  perMinuteNofitication(minutesRemaining) {
+    // TODO: use minutesRemaining to choose the right per minute.
+    if (minutesRemaining > 5) {
+      return pickRandomElement(this.allPlayers[COUNT_DOWN_VOICE])
+    }
+    return pickRandomElement(this.allPlayers[COUNT_DOWN_VOICE])
+  }
+
+  abrupt() {
+    return pickRandomElement(this.allPlayers[ABRUPT_VOICE])
+  }
+
+  giveup() {
+    return pickRandomElement(this.allPlayers[GIVE_UP_VOICE])
+  }
+
+  lastMinute() {
+    return pickRandomElement(this.allPlayers(LAST_MINUTE_VOICE))
+  }
+
+  fast() {
+    return pickRandomElement(this.allPlayers[FAST_VOICE])
+  }
+
+  shicoFast() {
+    return pickRandomElement(this.allPlayers[SHICO_FAST_VOICE])
+  }
+
+  countDown() {
+    return this.allPlayers[SINGLE_FILE_COUNT_DOWN_VOICE][0]
+  }
+
+  end() {
+    return pickRandomElement(this.allPlayers[END_VOICE])
+  }
+
+  exit() {
+    return pickRandomElement(this.allPlayers[EXIT_VOICE])
   }
 }
 
 class AssetFinder {
-  constructor (assetDir) {
+  constructor(assetDir) {
     this.dir = assetDir
-    fs.readdir(this.dir, function (dir) {
-      console.log(dir)
-      for (const filePath of dir) {
-        console.log(filePath)
-      }
-    })
   }
 
-  findAudioAssetGroups () {
-    return []
+  // TODO: Consider returning a promise here so that it doesn't need to be
+  // synchronous.
+  findAudioAssetGroups() {
+    const files = fs.readdirSync(this.dir)
+
+    const assetGroups = []
+    for (const filename of files) {
+      if (!filename.startsWith(SHIKO))
+        continue;
+
+      const assetNumber = filename.substr(SHIKO.length)
+      const voicename = VOICE + assetNumber
+      files.includes(voicename)
+      const shicoDir = path.join(this.dir, filename)
+      const voiceDir = path.join(this.dir, voicename)
+
+      assetGroups.push(new AudioAssetGroup(shicoDir, voiceDir))
+    }
+    return assetGroups
   }
 }
 
--- test/audioasset_test.js
+++ test/audioasset_test.js
@@ -1,18 +1,82 @@
 var assert = require('assert')
-const audioasset = require('../src/components/audioasset')
+var audioasset = require('../src/components/audioasset')
+const fs = require('fs')
+const sinon = require('sinon')
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1)
-    })
-  })
-})
+var chai = require('chai')
+  , should = chai.should()
+  , expect = chai.expect;
 
 describe('AudioAsset', function () {
-  describe('find', function () {
-    it('should find dires', function () {
-      new audioasset.AssetFinder('file:///media/psf/ASSAM/dmm/d_072497')
+  before(function () {
+    const readDirMap = {
+      '/root': ['shiko01', 'voice01', 'shiko02', 'voice02'],
+      '/root/shiko01': ['fast', 'finish', 'nomal', 'start'],
+      '/root/shiko01/fast': ['f92.wav'],
+      '/root/shiko01/finish': ['count.wav', 'end01.wav', 'za02.wav', 'zb08.wav'],
+      '/root/shiko01/nomal': ['01.wav', '12.wav'],
+      '/root/shiko01/start': [
+        'cdownxxx1.wav', 'cdown02.wav',
+        's01.wav', 'sf01.wav', 'sg01.wav', 'sz01.wav'],
+      '/root/voice01': ['fast', 'muon', 'nomal_s'],
+      '/root/voice01/fast': ['vf01.wav'],
+      '/root/voice01/muon': ['01.wav'],
+      '/root/voice01/nomal_s': ['v01.wav'],
+      '/root/shiko02': ['fast', 'finish', 'nomal', 'start'],
+      '/root/shiko02/fast': ['f92.wav'],
+      '/root/shiko02/finish': ['count.wav', 'end01.wav', 'za02.wav', 'zb08.wav'],
+      '/root/shiko02/nomal': ['01.wav', '12.wav'],
+      '/root/shiko02/start': [
+        'cdownxxx1.wav', 'cdown02.wav',
+        's01.wav', 'sf01.wav', 'sg01.wav', 'sz01.wav'],
+      '/root/voice02': ['fast', 'muon', 'nomal_s'],
+      '/root/voice02/fast': ['vf01.wav'],
+      '/root/voice02/muon': ['01.wav'],
+      '/root/voice02/nomal_s': ['v01.wav']
+    }
+    sinon.replace(fs, 'readdirSync', function (path) {
+      return readDirMap[path]
+    })
+  })
+  after(function () {
+    sinon.restore();
+  })
+
+  describe('AssetFinder', function () {
+    it('find 2 asset groups', function () {
+      const stub = sinon.stub().callsFake();
+      Object.setPrototypeOf(audioasset.AudioAssetGroup, stub);
+      const finder = new audioasset.AssetFinder('/root')
+      const groups = finder.findAudioAssetGroups()
+      assert.equal(groups.length, 2)
+      const groupAssetNumbers = []
+      for (let group of groups) {
+        groupAssetNumbers.push(group.assetGroupNumber())
+      }
+    })
+  })
+
+  describe('Assets', function () {
+    this.beforeEach(function () {
+      this.group = new audioasset.AudioAssetGroup(
+        '/root/shiko01', '/root/voice01')
+    })
+
+    it('find fast shiko', function () {
+      const voice = this.group.shicoFast()
+      expect(voice).to.not.be.false
+      expect(voice.pathForTesting).to.equal('/root/shiko01/fast/f92.wav')
+    })
+
+    it('find normal phrase', function () {
+      const voice = this.group.phrase()
+      expect(voice).to.not.be.false
+      expect(voice.pathForTesting).to.equal('/root/voice01/nomal_s/v01.wav')
+    })
+
+    it('get asset group number to be 1', function () {
+      const groupNumber = this.group.assetGroupNumber()
+      expect(groupNumber).to.equal(1)
     })
   })
 })
